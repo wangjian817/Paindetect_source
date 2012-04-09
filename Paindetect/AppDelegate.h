dbfd7203fcbc557be0faf51ec3455bdc8bb6f598
jegan selvaraj <jeganit@jegan-selvarajs-iMac-Venkatesh.local>
2012-04-09 16:51:51 +0530

    Initial Commit

---
+++ Paindetect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paindetect
+//
+//  Created by jegan selvaraj on 4/9/12.
+//  Copyright (c) 2012 blisslogix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
